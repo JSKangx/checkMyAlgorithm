0647d429597c7dba577e58889d334f9f11bb4630
Jinsu <injnamek@gmail.com>
2024-11-19 16:43:29 +0900

    11/19 알고리즘

--- lv_1/sumTwoNo.js
+++ lv_1/sumTwoNo.js
@@ -7,15 +7,32 @@
   필요한 알고리즘
   1. a, b 사이의 숫자를 순회하는 반복문
   2. a가 b보다 클 때 두 숫자 사이의 정수를 구하는 방법
-  2. 숫자를 더하는 방법
+    - 배열로 만들어 sort() 했다.
+  3. 숫자를 더하는 방법
 */
 
 const solution = (a, b) => {
   let answer = 0;
 
-  for (let i = a; i <= b; i++) {
+  let arr = [a, b];
+  arr.sort((a, b) => a - b);
+
+  for (let i = arr[0]; i <= arr[1]; i++) {
     answer += i;
   }
 
   return answer;
 };
+
+// 참고할만한 다른 풀이 : Math.min() / Math.max()
+const solution2 = (a, b) => {
+  let answer = 0;
+
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer += i;
+  }
+
+  return answer;
+};
+
+// Math.min, Math.max는 매개변수로 전달된 숫자들에서 최소, 최대값을 찾아주는 메서드다.
